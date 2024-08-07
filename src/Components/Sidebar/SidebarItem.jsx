0f80edd1efc24eee800f60b2389fd99351b8b467
evarellapucky <scherer.alexia@gmail.com>
2024-08-07 22:21:17 +0200

    prop types !

--- src/Components/Sidebar/SidebarItem.jsx
+++ src/Components/Sidebar/SidebarItem.jsx
@@ -1,4 +1,5 @@
 import { Link } from 'react-router-dom';
+import PropTypes from 'prop-types';
 
 const SidebarItem = ({ link, text, icon, textColor = 'text-white' }) => (
   <li className="p-2 hover:bg-blue-700 rounded">
@@ -8,5 +9,16 @@ const SidebarItem = ({ link, text, icon, textColor = 'text-white' }) => (
     </Link>
   </li>
 );
+
+SidebarItem.propTypes = {
+  link: PropTypes.string.isRequired,  // link must be a string and is required
+  text: PropTypes.string.isRequired,  // text must be a string and is required
+  icon: PropTypes.node.isRequired,    // icon can be any renderable node (e.g., JSX element)
+  textColor: PropTypes.string         // textColor is optional and must be a string
+};
+
+SidebarItem.defaultProps = {
+  textColor: 'text-white' // Default value for textColor
+};
 
 export default SidebarItem;
