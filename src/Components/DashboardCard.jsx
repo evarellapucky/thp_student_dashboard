6942718cece7e53a1deded1f11b9069fb3d4501f
Marcaraph <ra.marcarini@gmail.com>
2024-08-08 12:18:33 +0200

    First components dashboard

--- src/Components/DashboardCard.jsx
+++ src/Components/DashboardCard.jsx
@@ -9,7 +9,7 @@ function DashboardCards() {
 
   return (
     <>
-      <div className="grid grid-cols-1 sm:grid-cols-2 lg:grid-cols-4 p-2 w-full">
+      <div className="flex flex-row justify-around">
         <Journey />
         <Points />
         <MyJokers />
