0460e181a3dca7fb16e596ed0e652772af459141
Ton nom d'utilisateur githubYannRZG <yann.rezigui@outlook.fr>
2024-08-08 10:34:57 +0200

    todayTab

--- src/Components/Sidebar.jsx
+++ src/Components/Sidebar.jsx
@@ -3,12 +3,12 @@ import { Link } from 'react-router-dom'
 
 function Sidebar() {
   return (
-    <>
+    <div>
     <Link to="/dashboard">Dashboard</Link>
     <Link to="/today">Aujourd'hui</Link>
     <Link to="/agenda">Agenda</Link>
     <Link to="/profile">Profil</Link>
-    </>
+    </div>
   )
 }
 
