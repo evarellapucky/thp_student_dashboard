0f33d4b07ce0e44fb460fe9bdb53f6f6e1dd6485
tommy-pellerin <tommytp@hotmail.fr>
2024-08-08 11:18:29 +0200

    on going

--- src/Pages/Profile.jsx
+++ src/Pages/Profile.jsx
@@ -1,6 +1,7 @@
 import React from "react";
 import MyProfile from "../Components/MyProfile";
 import MyDocuments from "../Components/MyDocuments";
+import Leaderboard from "../Components/Leaderboard";
 
 function Profile() {
   return (
@@ -17,7 +18,7 @@ function Profile() {
 
       <input type="radio" name="my_tabs_2" role="tab" className="tab" aria-label="Leaderboard" />
       <div role="tabpanel" className="tab-content bg-base-100 border-base-300 rounded-box p-6">
-        Tab content 3
+        <Leaderboard/>
       </div>
 
       <input type="radio" name="my_tabs_2" role="tab" className="tab" aria-label="Ambassadeur" />
