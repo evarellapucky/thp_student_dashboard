2a9e2b25d79b6e489f78275d65db489364a9aa00
tommy-pellerin <tommytp@hotmail.fr>
2024-08-12 14:13:13 +0200

    whitespace debug

--- src/Pages/Profile.jsx
+++ src/Pages/Profile.jsx
@@ -11,7 +11,7 @@ function Profile() {
     <div role="tablist" className="tabs tabs-lifted">
       <input type="radio" name="my_tabs_2" role="tab" className="tab" aria-label="Profil" defaultChecked />
       <div role="tabpanel" className="tab-content bg-base-100 border-base-300 rounded-box p-6">
-        <MyProfile  />
+        <MyProfile/>
       </div>
 
       <input type="radio" name="my_tabs_2" role="tab" className="tab" aria-label="Documents" />
@@ -29,7 +29,7 @@ function Profile() {
         Tab content 4
       </div>
 
-      <input type="radio" name="my_tabs_2" role="tab" className="tab" aria-label="Mon parcours"/>
+      <input type="radio" name="my_tabs_2" role="tab" className="tab whitespace-nowrap" aria-label="Mon parcours"/>
       <div role="tabpanel" className="tab-content bg-base-100 border-base-300 rounded-box p-6">
         <MyJourney/>
       </div>
