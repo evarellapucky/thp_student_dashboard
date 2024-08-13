f50071d57b9a70b001aef4df322b353904d33fe5
tommy-pellerin <tommytp@hotmail.fr>
2024-08-13 11:00:53 +0200

    clean unuse table

--- src/Components/Profile/Directory.jsx
+++ src/Components/Profile/Directory.jsx
@@ -39,13 +39,6 @@ function Directory() {
 
   }, []);
 
-  // const data = [
-  //   { eleve:"Lili Daniels", role:"Manager", entreprise:"The hacking project", promo:"hiver 2024", github:"<a href='https://github.com/lilidaniels' class='hover:underline'>lilidaniels</a>", linkedin:"<a href='https://linkedin.com/in/lilidaniels' class='hover:underline'>lilidaniels</a>" },
-  //   { eleve:"James", role:"Developer", entreprise:"Tech Corp", promo:"été 2023", github:"<a href='https://github.com/james' class='hover:underline'>james</a>", linkedin:"<a href='https://linkedin.com/in/james' class='hover:underline'>james</a>" },
-  //   { eleve:"Seth McDaniel", role:"Designer", entreprise:"Facebook", promo:"printemps 2024", github:"<a href='https://github.com/sethmc' class='hover:underline'>setmc</a>", linkedin:"<a href='https://linkedin.com/in/SethMcDaniel' class='hover:underline'>Seth McDaniel</a>" },
-  //   { eleve:"Edward King", role:"Product owner", entreprise:"Doctolib", promo:"été 2019", github:"<a href='https://github.com/eking' class='hover:underline'>eKing</a>", linkedin:"<a href='https://linkedin.com/in/EdwardKing' class='hover:underline'>Edward King</a>" },
-  // ];
-
   //fonction pour afficher les liste filtrée sur ma promo ou non
   const toggleFilter = () => {
     setIsFiltered(!isFiltered);
