dc489df9178b55869fe209d18806ff2036279b3d
tommy-pellerin <tommytp@hotmail.fr>
2024-08-13 14:18:46 +0200

    first presentation

--- src/Components/Profile/Ambassador.jsx
+++ src/Components/Profile/Ambassador.jsx
@@ -2,7 +2,28 @@ import React from "react";
 
 function Ambassador() {
   return(
-    <h1>Ambassadeur</h1>
+    <>
+      <h1>Ambassadeur</h1>
+      <div>
+        <div className="flex justify-around my-5">
+          <div className="border-2 flex gap-5 p-5">
+            <p>Nombre de point THP gagnés :</p>
+            <p>3500</p>
+          </div>
+          <div className="border-2 flex gap-5 p-5">
+            <p>Montant de ma gagnotte :</p>
+            <p>350€</p>
+          </div>
+          <div className="border-2 flex gap-5 p-5">
+            <p>Nombre de filleuls :</p>          
+            <p>4</p>
+          </div>
+        </div>
+        <div>
+          <h2>Liste de parrainage</h2>
+        </div>
+      </div>
+    </>
   )
 }
 
