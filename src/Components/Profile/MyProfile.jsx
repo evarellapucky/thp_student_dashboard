d90bb4d3da50838b321012d7477bcffb5b49ad8c
tommy-pellerin <tommytp@hotmail.fr>
2024-08-14 14:09:53 +0200

    on going

--- src/Components/Profile/MyProfile.jsx
+++ src/Components/Profile/MyProfile.jsx
@@ -1,10 +1,17 @@
 import React from "react";
 import { useState } from "react";
 import { useDropzone } from 'react-dropzone';
-
+import Modal from "../Modal";
 function MyProfile() {
   const [showDropzone, setShowDropzone] = useState(false);
   const [imagePreview, setImagePreview] = useState(null);
+  const [isModalOpen, setIsModalOpen] = useState(false);
+  const [modalContent, setModalContent] = useState(null);
+
+  const handleOpenModal = () => {
+    setModalContent(<p>Contenu spécifique pour Projets THP</p>);
+    setIsModalOpen(true);
+  };
 
   const handleButtonClick = () => {
     setShowDropzone(!showDropzone);
@@ -106,7 +113,7 @@ function MyProfile() {
       </div>
 
       <div className="flex flex-wrap">
-        <button className="flex border-2 p-2">
+        <button className="flex border-2 p-2" onClick={handleOpenModal}>
           <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" strokeWidth="1.5" stroke="currentColor" className="size-6">
             <path strokeLinecap="round" strokeLinejoin="round" d="m16.862 4.487 1.687-1.688a1.875 1.875 0 1 1 2.652 2.652L10.582 16.07a4.5 4.5 0 0 1-1.897 1.13L6 18l.8-2.685a4.5 4.5 0 0 1 1.13-1.897l8.932-8.931Zm0 0L19.5 7.125M18 14v4.75A2.25 2.25 0 0 1 15.75 21H5.25A2.25 2.25 0 0 1 3 18.75V8.25A2.25 2.25 0 0 1 5.25 6H10" />
           </svg>
@@ -128,7 +135,7 @@ function MyProfile() {
           Supprimer mon profil
         </button>
       </div>
-      
+      {isModalOpen && <Modal isOpen={isModalOpen} onClose={() => setIsModalOpen(false)} content={modalContent} />}
     </>
   )
 }
