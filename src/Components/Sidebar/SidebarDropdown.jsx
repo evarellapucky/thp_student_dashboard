9747b3dc09fb351ec929036dcde0e9f70661bdb8
Korblen <malo.bastianelli@gmail.com>
2024-08-14 15:48:08 +0200

    bouton en haut + fenetre sur le coté

--- src/Components/Sidebar/SidebarDropdown.jsx
+++ src/Components/Sidebar/SidebarDropdown.jsx
@@ -1,7 +1,7 @@
+import React from 'react';
 import { useState } from "react";
 import PropTypes from "prop-types";
 import toggler_logo from "../../Assets/toggler.svg";
-
 
 const SidebarDropdown = ({ title, icon, isSidebarMinimized, children }) => {
   const [isOpen, setIsOpen] = useState(false);
@@ -15,31 +15,42 @@ const SidebarDropdown = ({ title, icon, isSidebarMinimized, children }) => {
       <li
         className={`p-2 rounded cursor-pointer transition-transform duration-200 ${
           isSidebarMinimized ? "hover:scale-125" : "hover:bg-blue-700"
-        }`}
+        } relative`}
         onClick={handleToggle}
       >
         <div className="flex items-center">
           {icon}
           {!isSidebarMinimized && (
-            <span className={`flex items-center text-white justify-between w-full ml-2 transition-all duration-300 ${isSidebarMinimized ? "hidden" : "inline-block"} whitespace-nowrap overflow-hidden`}>
+            <span
+              className={`flex items-center text-white justify-between w-full ml-2 transition-all duration-300 ${
+                isSidebarMinimized ? "hidden" : "inline-block"
+              } whitespace-nowrap overflow-hidden`}
+            >
               {title}
               <img
-                  src={toggler_logo}
-                  alt="toggle"
-                  className={`w-4 h-4 ml-2 transition-transform duration-300 ${
-                    isOpen ? "rotate-180" : ""
-                  }`}
-                />
-
+                src={toggler_logo}
+                alt="toggle"
+                className={`w-4 h-4 ml-2 transition-transform duration-300 ${
+                  isOpen ? "rotate-180" : ""
+                }`}
+              />
             </span>
           )}
         </div>
+
+        {isSidebarMinimized && isOpen && (
+          <div className="absolute left-full top-0 ml-2 bg-gray-800 p-2 rounded shadow-lg z-50">
+            <ul className="space-y-1 text-white">
+              {React.Children.map(children, (child) =>
+                React.cloneElement(child, { isSidebarMinimized: false })
+              )}
+            </ul>
+          </div>
+        )}
       </li>
+
       {!isSidebarMinimized && isOpen && (
-        <ul className="ml-6 space-y-1">{children}</ul>
-      )}
-      {isSidebarMinimized && isOpen && (
-        <ul >{children}</ul>
+        <ul className="ml-6 space-y-1 text-white">{children}</ul>
       )}
     </>
   );
