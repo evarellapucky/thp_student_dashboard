c3cc30343a3de84bbd04054cf160e387f6506b33
tommy-pellerin <tommytp@hotmail.fr>
2024-08-14 13:14:25 +0200

    more responsive

--- src/Components/Profile/MyProfile.jsx
+++ src/Components/Profile/MyProfile.jsx
@@ -38,30 +38,32 @@ function MyProfile() {
   return(
     <>
       <h1>Mon profil</h1>
-      <div className="grid grid-cols-3">
-        <div className="col-span-2 grid grid-cols-2 p-5 m-5">
-          <div>
-            <h4>Prénom</h4>
-            <p>Samantha</p>
-          </div>
+      <div className="grid grid-cols-1 md:grid-cols-3 gap-4 CheckboxGroup">
+        <div className="col-span-1 md:col-span-2">
+          <div className="grid grid-cols-1 md:grid-cols-2 gap-4 p-5 m-5">
+            <div>
+              <h4>Prénom</h4>
+              <p>Samantha</p>
+            </div>
 
-          <div>
-            <h4>Nom</h4>
-            <p>Belzebuth Baiocchi-pizza</p>
-          </div>
+            <div>
+              <h4>Nom</h4>
+              <p>Belzebuth Baiocchi-pizza</p>
+            </div>
 
-          <div>
-            <h4>Ville</h4>
-            <p>Satan-sur-Marne</p>
-          </div>
+            <div>
+              <h4>Ville</h4>
+              <p>Satan-sur-Marne</p>
+            </div>
 
-          <div>
-            <h4>Téléphone</h4>
-            <p>+66 6 666 666</p>
+            <div>
+              <h4>Téléphone</h4>
+              <p>+66 6 666 666</p>
+            </div>
           </div>
         </div>
 
-        <div className="text-center">
+        <div className="text-center p-5">
             <div className="h-72 w-full overflow-hidden">
               <img src={imagePreview || "https://img.daisyui.com/images/stock/photo-1534528741775-53994a69daeb.webp"} alt="Avatar" className="w-full h-full object-cover" />
             </div>
@@ -103,7 +105,7 @@ function MyProfile() {
         
       </div>
 
-      <div className="flex">
+      <div className="flex flex-wrap">
         <button className="flex border-2 p-2">
           <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" strokeWidth="1.5" stroke="currentColor" className="size-6">
             <path strokeLinecap="round" strokeLinejoin="round" d="m16.862 4.487 1.687-1.688a1.875 1.875 0 1 1 2.652 2.652L10.582 16.07a4.5 4.5 0 0 1-1.897 1.13L6 18l.8-2.685a4.5 4.5 0 0 1 1.13-1.897l8.932-8.931Zm0 0L19.5 7.125M18 14v4.75A2.25 2.25 0 0 1 15.75 21H5.25A2.25 2.25 0 0 1 3 18.75V8.25A2.25 2.25 0 0 1 5.25 6H10" />
