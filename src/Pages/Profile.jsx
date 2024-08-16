6783301aa3ccd184c2898aa9840a7d5661d0b05e
tommy-pellerin <tommytp@hotmail.fr>
2024-08-16 09:41:24 +0200

    better responsive

--- src/Pages/Profile.jsx
+++ src/Pages/Profile.jsx
@@ -13,6 +13,7 @@ function Profile() {
   const [selectedDailyTab, setSelectedDailyTab] = useState('handshakes');
 
   return (
+    
     <div role="tablist" className="tabs tabs-lifted">
       <input type="radio" name="my_tabs_2" role="tab" className="tab" aria-label="Profil" defaultChecked />
       <div role="tabpanel" className="tab-content bg-base-100 border-base-300 rounded-box p-6">
