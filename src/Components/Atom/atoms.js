97634e33c857532b17a322963f6ab1712ec13d54
evarellapucky <scherer.alexia@gmail.com>
2024-08-21 15:10:21 +0200

    create atom with storage

--- src/Components/Atom/atoms.js
+++ src/Components/Atom/atoms.js
@@ -1,6 +1,8 @@
 import { atom } from 'jotai';
+import { atomWithStorage } from 'jotai/utils';
 
 export const modalOpenAtom = atom(false);
 export const modalContentAtom = atom(null);
 export const totalMissionCountAtom = atom(0);
 export const issuesAtom = atom([]);
+export const favoritesAtom = atomWithStorage('favorites', []);
