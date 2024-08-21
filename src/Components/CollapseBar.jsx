35f2fded315be44764fdda04acaaff09e0d1c67f
Korblen <malo.bastianelli@gmail.com>
2024-08-21 11:36:21 +0200

    hein

--- src/Components/CollapseBar.jsx
+++ src/Components/CollapseBar.jsx
@@ -2,11 +2,11 @@ import PropTypes from 'prop-types';
 
 function CollapseBar({ title, content, borderColor }) {
   return (
-    <details className={`collapse collapse-arrow bg-base-200 mb-4 border ${borderColor} w-full max-w-lg md:min-w-full lg:min-w-full xl:min-w-full 2xl:min-w-full mx-auto`}>
-      <summary className="collapse-title text-lg md:text-xl font-medium bg-gray-200 p-4">
+    <details className={`collapse collapse-arrow bg-base-200 mb-4 border ${borderColor} w-full max-w-lg md:min-w-full lg:min-w-full xl:min-w-full 2xl:min-w-full mx-auto `}>
+      <summary className="collapse-title text-lg md:text-xl font-medium bg-gray-200 p-4 ">
         {title}
       </summary>
-      <div className="collapse-content bg-white p-4">
+      <div className="collapse-content bg-white p-4 ">
         <div className="text-sm md:text-base">
           {content}
         </div>
