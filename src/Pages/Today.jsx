0438c6126eb16547d52bfd049ff417242a4e5a3c
Ton nom d'utilisateur githubYannRZG <yann.rezigui@outlook.fr>
2024-08-21 23:48:38 +0200

    today and countdown

--- src/Pages/Today.jsx
+++ src/Pages/Today.jsx
@@ -14,7 +14,7 @@ const Today = () => {
       .then(response => {
         const data = response.data;
 
-        if (data && data.resources) { // Vérifiez que resources existent
+        if (data && data.resources) { 
           setResources(data.resources);
         } else {
           console.error('Resources non trouvés dans la réponse:', response.data);
@@ -23,40 +23,33 @@ const Today = () => {
       .catch(error => console.error('Erreur lors de la récupération des données:', error));
   }, []);
 
-  // Déterminer le mode de countdown en fonction de dayState
   const countdownMode = dayState === 'withSubmission'
     ? 'untilMidnight'
     : dayState === 'correction'
     ? 'untilNoon'
     : null;
 
-  // Log pour vérifier le mode de countdown
-  console.log('Countdown Mode:', countdownMode);
-
-  // Fonction appelée lorsque le compte à rebours se termine
   const handleCountdownEnd = () => {
     setIsCountdownActive(false);
   };
 
   return (
     <>
-      {/* Afficher Countdown seulement si dayState est 'withSubmission' ou 'correction' */}
-      {countdownMode && isCountdownActive && (
-        <div className='flex justify-end'>
-          <Countdown mode={countdownMode} onCountdownEnd={handleCountdownEnd} />
-        </div>
-      )}
-
       {dayState === 'withSubmission' && (
-        <div>
+        <div className="flex flex-col items-center">
           <h1 className="text-2xl md:text-3xl font-bold text-center">Projet à rendre</h1>
-          <div className="flex flex-col md:flex-row justify-center items-center mt-6 space-y-6 md:space-y-0 md:space-x-12">
+          {countdownMode && isCountdownActive && (
+            <div className="w-full flex justify-center mt-4">
+              <Countdown mode={countdownMode} onCountdownEnd={handleCountdownEnd} />
+            </div>
+          )}
+          <div className="flex flex-col md:flex-row justify-center items-center mt-6 space-y-6 md:space-y-0 md:space-x-12 ">
             <div className="flex flex-col items-center md:flex-row md:justify-end">
-              <div className="rounded-lg bg-base-200 p-4 flex flex-col space-y-2 w-80 sm:w-96">
+              <div className="rounded-lg bg-base-200 p-4 flex flex-col space-y-2 sm:w-96">
                 <InputField placeholder="Le nom du fichier" />
                 <InputField placeholder="Le lien du fichier" />
               </div>
-              <div className="flex items-center mt-6 md:mt-0 md:ml-6">
+              <div className="flex items-center mt-2 md:mt-0 md:ml-6">
                 <button className="btn bg-green-500 btn-circle shadow-lg hover:bg-white hover:border-green-500 border border-transparent group">
                   <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" strokeWidth="1.5" stroke="white" className="size-6 group-hover:stroke-green-500">
                     <path strokeLinecap="round" strokeLinejoin="round" d="M12 4.5v15m7.5-7.5h-15" />
@@ -68,12 +61,16 @@ const Today = () => {
         </div>
       )}
 
-      {dayState === 'correction' && isCountdownActive && (
-        <div>
+      {dayState === 'correction' && (
+        <div className="flex flex-col items-center">
           <h1 className="text-2xl md:text-3xl font-bold mt-8 mb-6 text-center">Corrections</h1>
+          {countdownMode && isCountdownActive && (
+            <div className="mt-4">
+              <Countdown mode={countdownMode} onCountdownEnd={handleCountdownEnd} />
+            </div>
+          )}
           <div className="flex flex-col items-center mt-6 space-y-6">
             <div className="w-full md:w-3/4 lg:w-2/3 grid grid-cols-1 sm:grid-cols-2 md:space-x-6 gap-4 mt-6 mb-8">
-              {/* Colonne pour corriger */}
               <div className="flex flex-col items-center p-4">
                 <h2 className="text-xl font-semibold mb-4">Corriger</h2>
                 {Array.from({ length: 2 }, (_, index) => (
@@ -84,7 +81,6 @@ const Today = () => {
                   </div>
                 ))}
               </div>
-              {/* Colonne pour être corrigé */}
               <div className="flex flex-col items-center space-y-4">
                 <h2 className="text-xl font-semibold mb-4">Être corrigé</h2>
                 {Array.from({ length: 2 }, (_, index) => (
@@ -100,8 +96,8 @@ const Today = () => {
         </div>
       )}
 
-      <div className="flex justify-center p-4">
-        <div className="w-full max-w-6xl">
+      <div className="flex justify-center mt-6 p-4 ">
+        <div className="w-full min-w-[95%]">
           {resources.map((resource, index) => (
             <CollapseBar 
               key={index}
