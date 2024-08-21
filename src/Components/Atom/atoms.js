42d51ed59370321abf45c81fc0806667a48b3d1b
evarellapucky <scherer.alexia@gmail.com>
2024-08-21 17:33:45 +0200

    change atom

--- src/Components/Atom/atoms.js
+++ src/Components/Atom/atoms.js
@@ -2,7 +2,7 @@ import { atom } from 'jotai';
 import { atomWithStorage } from 'jotai/utils';
 
 const initialToken = import.meta.env.VITE_GITHUB_TOKEN || '';
-import { atomWithStorage } from 'jotai/utils';
+
 
 export const modalOpenAtom = atom(false);
 export const modalContentAtom = atom(null);
