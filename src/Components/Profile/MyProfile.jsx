cbd1fa16c5ba1726dc035c87811da980f62d63c6
tommy-pellerin <tommytp@hotmail.fr>
2024-08-22 10:00:33 +0200

    better responsive myprofile

--- src/Components/Profile/MyProfile.jsx
+++ src/Components/Profile/MyProfile.jsx
@@ -87,8 +87,8 @@ function MyProfile() {
     <>
       <h1>Mon profil</h1>
       <div className="grid grid-cols-1 md:grid-cols-3 gap-4 CheckboxGroup">
-        <div className="col-span-1 md:col-span-2">
-          <div className="grid grid-cols-1 md:grid-cols-2 gap-4 p-5 m-5">
+        <div className="col-span-1 md:col-span-2 order-last md:order-first">
+          <div className="grid grid-cols-1 md:grid-cols-2 gap-4 p-5 m-1">
             <div>
               <h4>Prénom</h4>
               <p>{profileData.prenom}</p>
@@ -153,7 +153,7 @@ function MyProfile() {
         
       </div>
 
-      <div className="flex flex-wrap gap-3">
+      <div className="flex flex-wrap gap-3 px-5">
         <button className="flex border-2 p-2" onClick={handleOpenEditProfileModal}>
           <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" strokeWidth="1.5" stroke="currentColor" className="size-6">
             <path strokeLinecap="round" strokeLinejoin="round" d="m16.862 4.487 1.687-1.688a1.875 1.875 0 1 1 2.652 2.652L10.582 16.07a4.5 4.5 0 0 1-1.897 1.13L6 18l.8-2.685a4.5 4.5 0 0 1 1.13-1.897l8.932-8.931Zm0 0L19.5 7.125M18 14v4.75A2.25 2.25 0 0 1 15.75 21H5.25A2.25 2.25 0 0 1 3 18.75V8.25A2.25 2.25 0 0 1 5.25 6H10" />
