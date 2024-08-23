11d87cb93a3dc5490d5e4e2d10b532f4fc7cfcce
tommy-pellerin <tommytp@hotmail.fr>
2024-08-23 19:06:40 +0200

    fix impace from default button

--- src/Components/DefaultButton.jsx
+++ src/Components/DefaultButton.jsx
@@ -2,7 +2,7 @@ import React from "react";
 
 function DefaultButton({children, name, color = "primary", ...props}) {
   return (
-      <button className={`btn btn-${color} ml-2 md:ml-4`} {...props}>
+      <button className={`btn btn-${color} ml-2 md:ml-3`} {...props}>
         {children}
         {name}
       </button>
