ed9c487475459ee676728a1e16f4cc281fd12cc0
Ton nom d'utilisateur githubYannRZG <yann.rezigui@outlook.fr>
2024-08-23 17:09:17 +0200

    prrrr

--- src/Components/DailyTab.jsx
+++ src/Components/DailyTab.jsx
@@ -42,44 +42,46 @@ const DailyTab = ({ onSelect }) => {
   };
 
   return (
-    <div className="flex flex-col items-center space-y-8">
-      <div className="flex flex-row space-x-48">
-        <div 
-          className={`rounded-box bg-base-100 border border-base-300 h-52 w-80 cursor-pointer ${selectedTab === 'handshakes' ? 'shadow-inner' : 'shadow-out'} `}
-          onClick={() => handleClick('handshakes')}
-        >
-          <div className='flex flex-col justify-center items-center h-full space-y-8'>
-            <h1 className="text-xl font-bold">Coups de Mains</h1>
-            <div className='flex flex-row space-x-4'>
-              <img src="/src/Public/Images/Handshake.png" className='w-14 h-14' alt="Handshake" />
-              <p className='text-3xl font-bold'>{handshakesData ? handshakesData.length : 0}</p>
-            </div>
-          </div>
+<div className="flex flex-col items-center space-y-48">
+  <div className="flex flex-col justify-center items-center md:flex-row space-y-8 md:space-y-0 md:space-x-8">
+    <div 
+      className={`rounded-box bg-base-100 border border-base-300 h-52 w-80 cursor-pointer ${selectedTab === 'handshakes' ? 'shadow-inner' : 'shadow-out'} `}
+      onClick={() => handleClick('handshakes')}
+    >
+      <div className='flex flex-col justify-center items-center h-full space-y-4 md:space-y-8'>
+        <h1 className="text-xl font-bold">Coups de Mains</h1>
+        <div className='flex flex-row space-x-4 items-center'>
+          <img src="/src/Public/Images/Handshake.png" className='w-14 h-14' alt="Handshake" />
+          <p className='text-3xl font-bold'>{handshakesData ? handshakesData.length : 0}</p>
         </div>
-        <div 
-          className={`rounded-box bg-base-100 border border-base-300 h-52 w-80 cursor-pointer ${selectedTab === 'difficults' ? 'shadow-inner' : 'shadow-out'} `}
-          onClick={() => handleClick('difficults')}
-        >
-          <div className='flex flex-col justify-center items-center h-full space-y-8'>
-            <h1 className="text-xl font-bold">Difficultés rencontrées</h1>
-            <div className='flex flex-col items-center space-y-4'>
-              {/* La jauge sera affichée en permanence ici */}
-              <div className="w-40 h-20 flex flex-col items-center justify-center">
-                <GaugeChart 
-                  id="gauge-chart2" 
-                  nrOfLevels={10} 
-                  percent={averageRate !== null ? averageRate / 5 : 0}  // Assurez-vous que le pourcentage est entre 0 et 1
-                  colors={['#FF5F6D', '#F4F403', '#02b025']} 
-                  arcWidth={0.3}
-                  hideText 
-                />
-                <p className="text-2xl font-bold mt-2">{averageRate !== null ? averageRate.toFixed(2) : '0.00'}</p> {/* Affiche la valeur moyenne sous la jauge */}
-              </div>
-            </div>
+      </div>
+    </div>
+    <div 
+      className={`rounded-box bg-base-100 border border-base-300 h-52 w-80 cursor-pointer ${selectedTab === 'difficults' ? 'shadow-inner' : 'shadow-out'} `}
+      onClick={() => handleClick('difficults')}
+    >
+      <div className='flex flex-col justify-center items-center h-full space-y-4 md:space-y-8'>
+        <h1 className="text-xl font-bold">Difficultés rencontrées</h1>
+        <div className='flex flex-col items-center space-y-4'>
+          {/* La jauge sera affichée en permanence ici */}
+          <div className="w-40 h-20 flex flex-col items-center justify-center">
+            <GaugeChart 
+              id="gauge-chart2" 
+              nrOfLevels={10} 
+              percent={averageRate !== null ? averageRate / 5 : 0}  // Assurez-vous que le pourcentage est entre 0 et 1
+              colors={['#FF5F6D', '#F4F403', '#02b025']} 
+              arcWidth={0.3}
+              hideText 
+            />
+            <p className="text-2xl font-bold mt-2">{averageRate !== null ? averageRate.toFixed(2) : '0.00'}</p> {/* Affiche la valeur moyenne sous la jauge */}
           </div>
         </div>
       </div>
     </div>
+  </div>
+</div>
+
+
   );
 };
 
