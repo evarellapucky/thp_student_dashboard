a93e435ab014baad1031b7f7cb0e4ec2887d88cc
evarellapucky <scherer.alexia@gmail.com>
2024-08-30 12:40:05 +0200

    Reformaté pour Tommy !

--- src/Routes/AppRoutes.jsx
+++ src/Routes/AppRoutes.jsx
@@ -16,32 +16,29 @@ import KitUI from "../Pages/KitUI";
 import CategoryDetail from "../Components/Faq/CategoryDetail";
 import Historique from "../Components/Shop/Historique";
 
-
 function AppRoutes() {
   return (
-       <Routes>
-              <Route path="/" element={<Dashboard />} />
-              <Route path="/dashboard" element={<Dashboard />} />
-              <Route path="/today" element={<Today />} />
-              <Route path="/agenda" element={<Agenda />} />
-              <Route path="/profile" element={<Profile />} />
-              <Route path="/missions" element={<Missions />} />
-              <Route path="/projets" element={<Projets />} />
-              <Route path="/contact" element={<Contact />} />
-              <Route path="/search" element={<Search />} />
-              <Route path="/favorites" element={<Favorites />} />
-              <Route path="/faq" element={<Faq />} />
-              <Route path="/faq/:categoryName" element={<CategoryDetail />} />
-              <Route path="/shop" element={<Shop />} />
-              <Route path="/historique" element={<Historique />} />
-              <Route path="/projects" element={<Projets />} />
-              <Route path="/resource" element={<Resource />} />
-              <Route path="/sign_in" element={<SignIn />} />
-
-              <Route path="/kit_ui" element={<KitUI />} />
-            </Routes>
-  )
+    <Routes>
+      <Route path="/" element={<Dashboard />} />
+      <Route path="/dashboard" element={<Dashboard />} />
+      <Route path="/today" element={<Today />} />
+      <Route path="/agenda" element={<Agenda />} />
+      <Route path="/profile" element={<Profile />} />
+      <Route path="/missions" element={<Missions />} />
+      <Route path="/projets" element={<Projets />} />
+      <Route path="/contact" element={<Contact />} />
+      <Route path="/search" element={<Search />} />
+      <Route path="/favorites" element={<Favorites />} />
+      <Route path="/faq" element={<Faq />} />
+      <Route path="/faq/:categoryName" element={<CategoryDetail />} />
+      <Route path="/shop" element={<Shop />} />
+      <Route path="/historique" element={<Historique />} />
+      <Route path="/projects" element={<Projets />} />
+      <Route path="/resource" element={<Resource />} />
+      <Route path="/sign_in" element={<SignIn />} />
+      <Route path="/kit_ui" element={<KitUI />} />
+    </Routes>
+  );
 }
 
-export default AppRoutes
-
+export default AppRoutes;
